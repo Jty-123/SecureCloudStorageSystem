3e083665ae89aa851370b7bbbb645a2e029cc985
Jty <m911381@126.com>
2025-02-07 17:39:16 +0800

    remove sceret

--- 4/server/4.2.8/SecureCloudStorageSystem/src/main/java/com/sklois/SecureCloudStorageSystem/securitycloud/SecurityCloudServiceAccessAccount.java
+++ 4/server/4.2.8/SecureCloudStorageSystem/src/main/java/com/sklois/SecureCloudStorageSystem/securitycloud/SecurityCloudServiceAccessAccount.java
@@ -6,6 +6,6 @@ public class SecurityCloudServiceAccessAccount {
 //    public static String securitycloudaccesscredidentid = "LTAI4FtS295ioXzjpPA1vJhk";
 //    public static String securitycloudaccesscredidentkey = "IxEFwa04kUPBeRVnCbdDEcpZTaFgi9";
     public static String securitycloudendpoint = "https://114.55.101.121:8843";
-    public static String securitycloudaccesscredidentid = "0acb02bc58cc4154b1ed6f76562f57ff";
-    public static String securitycloudaccesscredidentkey = "50d25e0dfb0c17cb4be3d9665caec2990a682fcf05c23d44c9c24afa333eb327";
+    public static String securitycloudaccesscredidentid = ""  // "0acb02bc58cc4154b1ed6f76562f57ff";
+    public static String securitycloudaccesscredidentkey = "" // "50d25e0dfb0c17cb4be3d9665caec2990a682fcf05c23d44c9c24afa333eb327";
 }
